ab6040f1f79ddd825f7643caf4c8e002fbdca867
Kristian Sloth Lauszus <lauszus@gmail.com>
2013-04-28 14:56:14 +0200

    Improved I2C function
    Taken from my Balancing robot:
    https://github.com/TKJElectronics/Balanduino/blob/master/Firmware/Baland
    uino/I2C.ino

--- IMU6DOF/MPU6050/Graph/application.macosx/source/Graph.java
+++ IMU6DOF/MPU6050/Graph/application.macosx/source/Graph.java
@@ -17,22 +17,15 @@ import java.io.IOException;
 public class Graph extends PApplet {
 
  
-Serial arduino; 
+Serial serial; 
 
-String stringGyroX;
-String stringGyroY;
+String stringGyroX, stringGyroY;
+String stringAccX, stringAccY;
+String stringCompX, stringCompY;
+String stringKalmanX, stringKalmanY;
 
-String stringAccX;
-String stringAccY;
-
-String stringCompX;
-String stringCompY;
-
-String stringKalmanX;
-String stringKalmanY;
-
-int width = 1200;
-int height = 800;
+final int width = 1200;
+final int height = 800;
 
 float[] gyroX = new float[width];
 float[] gyroY = new float[width];
@@ -46,240 +39,253 @@ float[] compY = new float[width];
 float[] kalmanX = new float[width];
 float[] kalmanY = new float[width];
 
+boolean drawValues;
+
 public void setup() {  
   size(width, height);
-  //println(arduino.list()); // Use this to print connected serial devices
-  arduino = new Serial(this, Serial.list()[0], 115200);
-  arduino.bufferUntil('\n'); // Buffer until line feed
+  println(Serial.list()); // Use this to print connected serial devices
+  serial = new Serial(this, Serial.list()[8], 115200);
+  serial.bufferUntil('\n'); // Buffer until line feed
 
   for (int i=0;i<width;i++) { // center all variables
     gyroX[i] = height/2;
     gyroY[i] = height/2;
     accX[i] = height/2;
     accY[i] = height/2;
-    compX[i] = height/2; 
+    compX[i] = height/2;
     compY[i] = height/2; 
-    kalmanX[i] = height/2; 
+    kalmanX[i] = height/2;
     kalmanY[i] = height/2;
+  }
+  
+  drawGraph(); // Draw graph at startup
+}
+
+public void draw() {
+  /* Draw Graph */
+  if(drawValues) {
+    drawValues = false;
+    drawGraph();
   }
 }
 
-public void draw()
-{ 
-  // Draw graphPaper
-  background(255); // white
-  for (int i = 0 ;i<=width/10;i++) {      
+public void drawGraph() {
+  background(255); // white  
+  for (int i = 0;i<width;i++) {
     stroke(200); // gray
-    line((-frameCount%10)+i*10, 0, (-frameCount%10)+i*10, height);
+    line(i*10, 0, i*10, height);
     line(0, i*10, width, i*10);
   }
-
+  
   stroke(0); // black
   for (int i = 1; i <= 3; i++)
     line(0, height/4*i, width, height/4*i); // Draw line, indicating 90 deg, 180 deg, and 270 deg
-
+  
   convert();
   drawAxisX();
   //drawAxisY();
 }
 
-public void serialEvent (Serial arduino) {
-  // get the ASCII strings:  
-  stringAccX = arduino.readStringUntil('\t');
-  stringAccY = arduino.readStringUntil('\t');
+public void serialEvent (Serial serial) {
+  // Get the ASCII strings:  
+  stringAccX = serial.readStringUntil('\t');
+  stringGyroX = serial.readStringUntil('\t');
+  stringCompX = serial.readStringUntil('\t');
+  stringKalmanX = serial.readStringUntil('\t');
   
-  stringGyroX = arduino.readStringUntil('\t');
-  stringGyroY = arduino.readStringUntil('\t');
-
-  stringCompX = arduino.readStringUntil('\t');
-  stringCompY = arduino.readStringUntil('\t'); 
-
-  stringKalmanX = arduino.readStringUntil('\t');
-  stringKalmanY = arduino.readStringUntil('\t');
+  serial.readStringUntil('\t'); // Ignore extra tab
   
-  arduino.clear(); // Clear buffer
+  stringAccY = serial.readStringUntil('\t');
+  stringGyroY = serial.readStringUntil('\t');
+  stringCompY = serial.readStringUntil('\t');
+  stringKalmanY = serial.readStringUntil('\t');
+  
+  serial.clear(); // Clear buffer
+  drawValues = true; // Draw the graph
 
-  //printAxis(); // slows down the process and can result in error readings - use for debugging
+  //printAxis(); // Used for debugging
 }
 
 public void printAxis() {
   print(stringGyroX);
-  print(stringGyroY);
-
   print(stringAccX);
-  print(stringAccY);
-
   print(stringCompX);
-  print(stringCompY);
-
   print(stringKalmanX);
+  
+  print('\t');
+  
+  print(stringGyroY);
+  print(stringAccY);
+  print(stringCompY);
   print(stringKalmanY);
+  
+  println();
 }
 //convert all axis
-int minAngle = 0;
-int maxAngle = 360;
+final int minAngle = 0;
+final int maxAngle = 360;
 
-public void convert() {   
-  /* convert the gyro x-axis */
+public void convert() {
+  /* Convert the gyro x-axis */
   if (stringGyroX != null) {
-    // trim off any whitespace:
+    // Trim off any whitespace:
     stringGyroX = trim(stringGyroX);
-    // convert to an float and map to the screen height, then save in buffer:    
+    // Convert to an float and map to the screen height, then save in buffer:    
     gyroX[gyroX.length-1] = map(PApplet.parseFloat(stringGyroX), minAngle, maxAngle, 0, height);
   }
   
-  /* convert the gyro y-axis */
+  /* Convert the gyro y-axis */
   if (stringGyroY != null) {    
-    // trim off any whitespace:
+    // Trim off any whitespace:
     stringGyroY = trim(stringGyroY);
-    // convert to an float and map to the screen height, then save in buffer:   
+    // Convert to an float and map to the screen height, then save in buffer:   
     gyroY[gyroY.length-1] = map(PApplet.parseFloat(stringGyroY), minAngle, maxAngle, 0, height);
   }
   
-  /* convert the accelerometer x-axis */
+  /* Convert the accelerometer x-axis */
   if (stringAccX != null) {
-    // trim off any whitespace:
+    // Trim off any whitespace:
     stringAccX = trim(stringAccX);
-    // convert to an float and map to the screen height, then save in buffer:    
+    // Convert to an float and map to the screen height, then save in buffer:    
     accX[accX.length-1] = map(PApplet.parseFloat(stringAccX), minAngle, maxAngle, 0, height);
   }
   
-  /* convert the accelerometer y-axis */
+  /* Convert the accelerometer y-axis */
   if (stringAccY != null) {
-    // trim off any whitespace:
+    // Trim off any whitespace:
     stringAccY = trim(stringAccY);
-    // convert to an float and map to the screen height, then save in buffer:        
+    // Convert to an float and map to the screen height, then save in buffer:        
     accY[accY.length-1] = map(PApplet.parseFloat(stringAccY), minAngle, maxAngle, 0, height);
   }
 
-  /* convert the complementary filter x-axis */
+  /* Convert the complementary filter x-axis */
   if (stringCompX != null) {
-    // trim off any whitespace:
+    // Trim off any whitespace:
     stringCompX = trim(stringCompX);
-    // convert to an float and map to the screen height, then save in buffer:    
+    // Convert to an float and map to the screen height, then save in buffer:    
     compX[compX.length-1] = map(PApplet.parseFloat(stringCompX), minAngle, maxAngle, 0, height);
   }
   
-  /* convert the complementary filter x-axis */
+  /* Convert the complementary filter x-axis */
   if (stringCompY != null) {
-    // trim off any whitespace:
+    // Trim off any whitespace:
     stringCompY = trim(stringCompY);
-    // convert to an float and map to the screen height, then save in buffer:    
+    // Convert to an float and map to the screen height, then save in buffer:    
     compY[compY.length-1] = map(PApplet.parseFloat(stringCompY), minAngle, maxAngle, 0, height);
   }
   
-  /* convert the kalman filter x-axis */
+  /* Convert the kalman filter x-axis */
   if (stringKalmanX != null) {
-    // trim off any whitespace:
+    // Trim off any whitespace:
     stringKalmanX = trim(stringKalmanX);
-    // convert to an float and map to the screen height, then save in buffer:    
+    // Convert to an float and map to the screen height, then save in buffer:    
     kalmanX[kalmanX.length-1] = map(PApplet.parseFloat(stringKalmanX), minAngle, maxAngle, 0, height);
   }
   
-  /* convert the kalman filter y-axis */
+  /* Convert the kalman filter y-axis */
   if (stringKalmanY != null) {
-    // trim off any whitespace:
+    // Trim off any whitespace:
     stringKalmanY = trim(stringKalmanY);
-    // convert to an float and map to the screen height, then save in buffer:    
+    // Convert to an float and map to the screen height, then save in buffer:    
     kalmanY[kalmanY.length-1] = map(PApplet.parseFloat(stringKalmanY), minAngle, maxAngle, 0, height);
   }
 }
 public void drawAxisX() {
-  /* draw gyro x-axis */
+  /* Draw gyro x-axis */
   noFill();
-  stroke(255,255,0); // yellow
-  // redraw everything
+  stroke(255,255,0); // Yellow
+  // Redraw everything
   beginShape();
   for(int i = 0; i<gyroX.length;i++)
     vertex(i,gyroX[i]);
   endShape();
-  // put all data one array back
+  // Put all data one array back
   for(int i = 1; i<gyroX.length;i++)
     gyroX[i-1] = gyroX[i];   
    
-  /* draw acceleromter x-axis */
+  /* Draw acceleromter x-axis */
   noFill();
-  stroke(0,255,0); // green
-  // redraw everything
+  stroke(0,255,0); // Green
+  // Redraw everything
   beginShape();
   for(int i = 0; i<accX.length;i++)
     vertex(i,accX[i]);  
   endShape();
-  // put all data one array back
+  // Put all data one array back
   for(int i = 1; i<accX.length;i++)
     accX[i-1] = accX[i];   
    
-  /* draw complementary filter x-axis */
+  /* Draw complementary filter x-axis */
   noFill();
-  stroke(0,0,255); // blue
-  // redraw everything
+  stroke(0,0,255); // Blue
+  // Redraw everything
   beginShape();
   for(int i = 0; i<compX.length;i++)
     vertex(i,compX[i]);
   endShape();
-  // put all data one array back
+  // Put all data one array back
   for(int i = 1; i<compX.length;i++)
     compX[i-1] = compX[i];  
    
-  /* draw kalman filter x-axis */
+  /* Draw kalman filter x-axis */
   noFill();
-  stroke(255,0,0);//red
-  // redraw everything
+  stroke(255,0,0);// Red
+  // Redraw everything
   beginShape();
   for(int i = 0; i<kalmanX.length;i++)
     vertex(i,kalmanX[i]);  
   endShape();
-  // put all data one array back
+  // Put all data one array back
   for(int i = 1; i<kalmanX.length;i++)
     kalmanX[i-1] = kalmanX[i];
 }
 
 public void drawAxisY() {
-  /* draw gyro y-axis */
+  /* Draw gyro y-axis */
   noFill();
-  stroke(255,255,0); // yellow
-  // redraw everything
+  stroke(255,255,0); // Yellow
+  // Redraw everything
   beginShape();
   for(int i = 0; i<gyroY.length;i++)
     vertex(i,gyroY[i]);
   endShape();
-  // put all data one array back
+  // Put all data one array back
   for(int i = 1; i<gyroY.length;i++)
    gyroY[i-1] = gyroY[i];
    
-  /* draw acceleromter y-axis */
+  /* Draw acceleromter y-axis */
   noFill();
-  stroke(0,255,0); // green
-  // redraw everything
+  stroke(0,255,0); // Green
+  // Redraw everything
   beginShape();
   for(int i = 0; i<accY.length;i++)
     vertex(i,accY[i]);
   endShape();
-  // put all data one array back
+  // Put all data one array back
   for(int i = 1; i<accY.length;i++)
     accY[i-1] = accY[i];
    
-  /* draw complementary filter y-axis */
+  /* Draw complementary filter y-axis */
   noFill();
-  stroke(0,0,255); // blue
-  // redraw everything
+  stroke(0,0,255); // Blue
+  // Redraw everything
   beginShape();
   for(int i = 0; i<compY.length;i++)
     vertex(i,compY[i]);
   endShape();
-  // put all data one array back
+  // Put all data one array back
   for(int i = 1; i<compY.length;i++)
     compY[i-1] = compY[i];
   
-  /* draw kalman filter y-axis */
+  /* Draw kalman filter y-axis */
   noFill();
-  stroke(255,0,0); // red
-  // redraw everything
+  stroke(255,0,0); // Red
+  // Redraw everything
   beginShape();
   for(int i = 0; i<kalmanY.length;i++)
     vertex(i,kalmanY[i]);
   endShape();
-  //put all data one array back
+  // Put all data one array back
   for(int i = 1; i<kalmanY.length;i++)
     kalmanY[i-1] = kalmanY[i];
 }    
--- IMU6DOF/MPU6050/MPU6050.ino
+++ IMU6DOF/MPU6050/MPU6050.ino
@@ -18,61 +18,75 @@
 #include <Wire.h>
 #include "Kalman.h" // Source: https://github.com/TKJElectronics/KalmanFilter
 
-Kalman kalmanX;
+Kalman kalmanX; // Create the Kalman instances
 Kalman kalmanY;
 
-const uint8_t IMUAddress = 0x68;
-
 /* IMU Data */
-int16_t accX;
-int16_t accY;
-int16_t accZ;
+int16_t accX, accY, accZ;
 int16_t tempRaw;
-int16_t gyroX;
-int16_t gyroY;
-int16_t gyroZ;
+int16_t gyroX, gyroY, gyroZ;
 
-double accXangle; // Angle calculate using the accelerometer
-double accYangle;
-double temp;
-double gyroXangle = 180; // Angle calculate using the gyro
-double gyroYangle = 180;
-double compAngleX = 180; // Calculate the angle using a Kalman filter
-double compAngleY = 180;
-double kalAngleX; // Calculate the angle using a Kalman filter
-double kalAngleY;
+double accXangle, accYangle; // Angle calculate using the accelerometer
+double temp; // Temperature
+double gyroXangle, gyroYangle; // Angle calculate using the gyro
+double compAngleX, compAngleY; // Calculate the angle using a complementary filter
+double kalAngleX, kalAngleY; // Calculate the angle using a Kalman filter
 
 uint32_t timer;
+uint8_t i2cData[14]; // Buffer for I2C data
 
 void setup() {  
   Serial.begin(115200);
-  Wire.begin();  
-  i2cWrite(0x6B,0x00); // Disable sleep mode  
-  if(i2cRead(0x75,1)[0] != 0x68) { // Read "WHO_AM_I" register
-    Serial.print(F("MPU-6050 with address 0x"));
-    Serial.print(IMUAddress,HEX);
-    Serial.println(F(" is not connected"));
+  Wire.begin();
+  while(i2cWrite(0x1B,0x00,false)); // Set Gyro Full Scale Range to ±250deg/s
+  while(i2cWrite(0x1C,0x00,false)); // Set Accelerometer Full Scale Range to ±2g
+  while(i2cWrite(0x19,7,false)); // Set the sample rate to 1000Hz
+  while(i2cWrite(0x1A,0x00,false)); // Disable FSYNC and set 260 Hz Acc filtering, 256 Hz Gyro filtering, 8 KHz sampling
+  while(i2cWrite(0x6B,0x01,true)); // PLL with X axis gyroscope reference and disable sleep mode 
+  
+  while(i2cRead(0x75,i2cData,1));
+  if(i2cData[0] != 0x68) { // Read "WHO_AM_I" register
+    Serial.print(F("Error reading sensor"));
     while(1);
-  }      
-  kalmanX.setAngle(180); // Set starting angle
-  kalmanY.setAngle(180);
+  }
+  
+  delay(100); // Wait for sensor to stabilize
+  
+  /* Set kalman and gyro starting angle */
+  while(i2cRead(0x3B,i2cData,6));
+  accX = ((i2cData[0] << 8) | i2cData[1]);
+  accY = ((i2cData[2] << 8) | i2cData[3]);
+  accZ = ((i2cData[4] << 8) | i2cData[5]);
+  // atan2 outputs the value of -π to π (radians) - see http://en.wikipedia.org/wiki/Atan2
+  // We then convert it to 0 to 2π and then from radians to degrees
+  accYangle = (atan2(accX,accZ)+PI)*RAD_TO_DEG;
+  accXangle = (atan2(accY,accZ)+PI)*RAD_TO_DEG;
+  
+  kalmanX.setAngle(accXangle); // Set starting angle
+  kalmanY.setAngle(accYangle);
+  gyroXangle = accXangle;
+  gyroYangle = accYangle;
+  compAngleX = accXangle;
+  compAngleY = accYangle;
+  
   timer = micros();
 }
 
 void loop() {
-  /* Update all the values */
-  uint8_t* data = i2cRead(0x3B,14);  
-  accX = ((data[0] << 8) | data[1]);
-  accY = ((data[2] << 8) | data[3]);
-  accZ = ((data[4] << 8) | data[5]);  
-  tempRaw = ((data[6] << 8) | data[7]);  
-  gyroX = ((data[8] << 8) | data[9]);
-  gyroY = ((data[10] << 8) | data[11]);
-  gyroZ = ((data[12] << 8) | data[13]);
+  /* Update all the values */  
+  while(i2cRead(0x3B,i2cData,14));
+  accX = ((i2cData[0] << 8) | i2cData[1]);
+  accY = ((i2cData[2] << 8) | i2cData[3]);
+  accZ = ((i2cData[4] << 8) | i2cData[5]);
+  tempRaw = ((i2cData[6] << 8) | i2cData[7]);  
+  gyroX = ((i2cData[8] << 8) | i2cData[9]);
+  gyroY = ((i2cData[10] << 8) | i2cData[11]);
+  gyroZ = ((i2cData[12] << 8) | i2cData[13]);
   
-  /* Calculate the angls based on the different sensors and algorithm */
+  // atan2 outputs the value of -π to π (radians) - see http://en.wikipedia.org/wiki/Atan2
+  // We then convert it to 0 to 2π and then from radians to degrees
+  accXangle = (atan2(accY,accZ)+PI)*RAD_TO_DEG;
   accYangle = (atan2(accX,accZ)+PI)*RAD_TO_DEG;
-  accXangle = (atan2(accY,accZ)+PI)*RAD_TO_DEG;    
   
   double gyroXrate = (double)gyroX/131.0;
   double gyroYrate = -((double)gyroY/131.0);
@@ -82,7 +96,7 @@ void loop() {
   //gyroYangle += kalmanY.getRate()*((double)(micros()-timer)/1000000);
   
   compAngleX = (0.93*(compAngleX+(gyroXrate*(double)(micros()-timer)/1000000)))+(0.07*accXangle); // Calculate the angle using a Complimentary filter
-  compAngleY = (0.93*(compAngleY+(gyroYrate*(double)(micros()-timer)/1000000)))+(0.07*accYangle);  
+  compAngleY = (0.93*(compAngleY+(gyroYrate*(double)(micros()-timer)/1000000)))+(0.07*accYangle);
   
   kalAngleX = kalmanX.getAngle(accXangle, gyroXrate, (double)(micros()-timer)/1000000); // Calculate the angle using a Kalman filter
   kalAngleY = kalmanY.getAngle(accYangle, gyroYrate, (double)(micros()-timer)/1000000);
@@ -90,47 +104,30 @@ void loop() {
   
   temp = ((double)tempRaw + 12412.0) / 340.0;
   
-  /* Print Data */   
+  /* Print Data */
   /*
   Serial.print(accX);Serial.print("\t");
-  Serial.print(accY);Serial.print("\t");  
-  Serial.print(accZ);Serial.print("\t");    
+  Serial.print(accY);Serial.print("\t");
+  Serial.print(accZ);Serial.print("\t");
   
-  Serial.print(gyroX);Serial.print("\t");  
-  Serial.print(gyroY); Serial.print("\t");   
-  Serial.print(gyroZ);Serial.print("\t");  
+  Serial.print(gyroX);Serial.print("\t");
+  Serial.print(gyroY); Serial.print("\t");
+  Serial.print(gyroZ);Serial.print("\t");
   */
   Serial.print(accXangle);Serial.print("\t");
-  Serial.print(accYangle);Serial.print("\t"); 
-    
   Serial.print(gyroXangle);Serial.print("\t");
-  Serial.print(gyroYangle);Serial.print("\t");
-  
   Serial.print(compAngleX);Serial.print("\t");
-  Serial.print(compAngleY); Serial.print("\t");
-  
   Serial.print(kalAngleX);Serial.print("\t");
+  
+  Serial.print("\t");
+  
+  Serial.print(accYangle);Serial.print("\t");
+  Serial.print(gyroYangle);Serial.print("\t");
+  Serial.print(compAngleY); Serial.print("\t");
   Serial.print(kalAngleY);Serial.print("\t");
   
   //Serial.print(temp);Serial.print("\t");
    
-  Serial.print("\n");
-  
-  delay(1); // The accelerometer's maximum samples rate is 1kHz
+  Serial.print("\r\n");
+  delay(1);
 }
-void i2cWrite(uint8_t registerAddress, uint8_t data){
-  Wire.beginTransmission(IMUAddress);
-  Wire.write(registerAddress);
-  Wire.write(data);
-  Wire.endTransmission(); // Send stop
-}
-uint8_t* i2cRead(uint8_t registerAddress, uint8_t nbytes) {
-  uint8_t data[nbytes];  
-  Wire.beginTransmission(IMUAddress);
-  Wire.write(registerAddress);
-  Wire.endTransmission(false); // Don't release the bus
-  Wire.requestFrom(IMUAddress, nbytes); // Send a repeated start and then release the bus after reading
-  for(uint8_t i = 0; i < nbytes; i++)
-    data[i] = Wire.read();
-  return data;
-} 
