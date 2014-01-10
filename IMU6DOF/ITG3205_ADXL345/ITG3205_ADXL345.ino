c6d4f29d488c80d9bfc1c76875138a849e18742c
Kristian Lauszus <lauszus@gmail.com>
2014-01-10 21:55:14 +0100

    Reformat code
    No code change

--- IMU6DOF/ITG3205_ADXL345/ITG3205_ADXL345.ino
+++ IMU6DOF/ITG3205_ADXL345/ITG3205_ADXL345.ino
@@ -1,15 +1,15 @@
 /* Copyright (C) 2012 Kristian Lauszus, TKJ Electronics. All rights reserved.
- 
+
  This software may be distributed and modified under the terms of the GNU
  General Public License version 2 (GPL2) as published by the Free Software
  Foundation and appearing in the file GPL2.TXT included in the packaging of
  this file. Please note that GPL2 Section 2[b] requires that all works based
  on this software must also be made publicly available under the terms of
  the GPL2 ("Copyleft").
- 
+
  Contact information
  -------------------
- 
+
  Kristian Lauszus, TKJ Electronics
  Web      :  http://www.tkjelectronics.com
  e-mail   :  kristianl@tkjelectronics.com
@@ -24,7 +24,7 @@ Kalman kalmanY;
 #define gyroAddress 0x68
 #define adxlAddress 0x53
 
-double zeroValue[5] = {-200, 44, 660, 52.3, -18.5}; // Found by experimenting
+double zeroValue[5] = { -200, 44, 660, 52.3, -18.5}; // Found by experimenting
 
 /* All the angles start at 180 degrees */
 double gyroXangle = 180;
@@ -35,103 +35,103 @@ double compAngleY = 180;
 
 unsigned long timer;
 
-void setup() {  
+void setup() {
   Serial.begin(115200);
   Wire.begin();
-  
-  i2cWrite(adxlAddress,0x31,0x09); // Full resolution mode
-  i2cWrite(adxlAddress,0x2D,0x08); // Setup ADXL345 for constant measurement mode
-  
-  i2cWrite(gyroAddress,0x16,0x1A); // this puts your gyro at +-2000deg/sec  and 98Hz Low pass filter
-  i2cWrite(gyroAddress,0x15,0x09); // this sets your gyro at 100Hz sample rate
+
+  i2cWrite(adxlAddress, 0x31, 0x09); // Full resolution mode
+  i2cWrite(adxlAddress, 0x2D, 0x08); // Setup ADXL345 for constant measurement mode
+
+  i2cWrite(gyroAddress, 0x16, 0x1A); // this puts your gyro at +-2000deg/sec  and 98Hz Low pass filter
+  i2cWrite(gyroAddress, 0x15, 0x09); // this sets your gyro at 100Hz sample rate
 
   kalmanX.setAngle(180); // Set starting angle
   kalmanY.setAngle(180);
   timer = micros();
 }
 
-void loop() {    
-  double gyroXrate = -(((double)readGyroX()-zeroValue[3])/14.375);
-  gyroXangle += gyroXrate*((double)(micros()-timer)/1000000); // Without any filter
-  
-  double gyroYrate = (((double)readGyroY()-zeroValue[4])/14.375);
-  gyroYangle += gyroYrate*((double)(micros()-timer)/1000000); // Without any filter
-  
+void loop() {
+  double gyroXrate = -(((double)readGyroX() - zeroValue[3]) / 14.375);
+  gyroXangle += gyroXrate * ((double)(micros() - timer) / 1000000); // Without any filter
+
+  double gyroYrate = (((double)readGyroY() - zeroValue[4]) / 14.375);
+  gyroYangle += gyroYrate * ((double)(micros() - timer) / 1000000); // Without any filter
+
   double accXangle = getXangle();
   double accYangle = getYangle();
-  
-  compAngleX = (0.93*(compAngleX+(gyroXrate*(double)(micros()-timer)/1000000)))+(0.07*accXangle);
-  compAngleY = (0.93*(compAngleY+(gyroYrate*(double)(micros()-timer)/1000000)))+(0.07*accYangle);
-  
+
+  compAngleX = (0.93 * (compAngleX + (gyroXrate * (double)(micros() - timer) / 1000000))) + (0.07 * accXangle);
+  compAngleY = (0.93 * (compAngleY + (gyroYrate * (double)(micros() - timer) / 1000000))) + (0.07 * accYangle);
+
   double xAngle = kalmanX.getAngle(accXangle, gyroXrate, (double)(micros() - timer)); // calculate the angle using a Kalman filter
   double yAngle = kalmanY.getAngle(accYangle, gyroYrate, (double)(micros() - timer)); // calculate the angle using a Kalman filter
-  
+
   timer = micros();
-  
+
   /* print data to processing */
-  Serial.print(gyroXangle);Serial.print("\t");
-  Serial.print(gyroYangle);Serial.print("\t");
-  
-  Serial.print(accXangle);Serial.print("\t");
-  Serial.print(accYangle);Serial.print("\t");
-  
-  Serial.print(compAngleX);Serial.print("\t");  
-  Serial.print(compAngleY); Serial.print("\t"); 
-  
-  Serial.print(xAngle);Serial.print("\t");
-  Serial.print(yAngle);Serial.print("\t");
-   
+  Serial.print(gyroXangle); Serial.print("\t");
+  Serial.print(gyroYangle); Serial.print("\t");
+
+  Serial.print(accXangle); Serial.print("\t");
+  Serial.print(accYangle); Serial.print("\t");
+
+  Serial.print(compAngleX); Serial.print("\t");
+  Serial.print(compAngleY); Serial.print("\t");
+
+  Serial.print(xAngle); Serial.print("\t");
+  Serial.print(yAngle); Serial.print("\t");
+
   Serial.print("\n");
-  
+
   delay(10);
 }
-void i2cWrite(uint8_t address, uint8_t registerAddress, uint8_t data){
+void i2cWrite(uint8_t address, uint8_t registerAddress, uint8_t data) {
   Wire.beginTransmission(address);
   Wire.write(registerAddress);
   Wire.write(data);
   Wire.endTransmission();
 }
 uint8_t* i2cRead(uint8_t address, uint8_t registerAddress, uint8_t nbytes) {
-  uint8_t data[nbytes];  
+  uint8_t data[nbytes];
   Wire.beginTransmission(address);
   Wire.write(registerAddress);
-  Wire.endTransmission();  
+  Wire.endTransmission();
   Wire.beginTransmission(address);
   Wire.requestFrom(address, nbytes);
-  for(uint8_t i = 0; i < nbytes; i++)
+  for (uint8_t i = 0; i < nbytes; i++)
     data[i] = Wire.read();
-  Wire.endTransmission();  
+  Wire.endTransmission();
   return data;
 }
 int readGyroX() { // This really measures the y-axis of the gyro
-  uint8_t* data = i2cRead(gyroAddress, 0x1F,2);    
+  uint8_t* data = i2cRead(gyroAddress, 0x1F, 2);
   return ((data[0] << 8) | data[1]);
 }
 int readGyroY() { // This really measures the x-axis of the gyro
-  uint8_t* data = i2cRead(gyroAddress, 0x1D,2);     
+  uint8_t* data = i2cRead(gyroAddress, 0x1D, 2);
   return ((data[0] << 8) | data[1]);
 }
 double getXangle() {
-  double accXval = (double)readAccX()-zeroValue[0];
-  double accZval = (double)readAccZ()-zeroValue[2];
-  double angle = (atan2(accXval,accZval)+PI)*RAD_TO_DEG;
+  double accXval = (double)readAccX() - zeroValue[0];
+  double accZval = (double)readAccZ() - zeroValue[2];
+  double angle = (atan2(accXval, accZval) + PI) * RAD_TO_DEG;
   return angle;
 }
 double getYangle() {
-  double accYval = (double)readAccY()-zeroValue[1];
-  double accZval = (double)readAccZ()-zeroValue[2];
-  double angle = (atan2(accYval,accZval)+PI)*RAD_TO_DEG;
+  double accYval = (double)readAccY() - zeroValue[1];
+  double accZval = (double)readAccZ() - zeroValue[2];
+  double angle = (atan2(accYval, accZval) + PI) * RAD_TO_DEG;
   return angle;
 }
 int readAccX() {
-  uint8_t* data = i2cRead(adxlAddress, 0x32,2);     
+  uint8_t* data = i2cRead(adxlAddress, 0x32, 2);
   return (data[0] | (data[1] << 8));
 }
 int readAccY() {
-  uint8_t* data = i2cRead(adxlAddress, 0x34,2);     
+  uint8_t* data = i2cRead(adxlAddress, 0x34, 2);
   return (data[0] | (data[1] << 8));
 }
 int readAccZ() {
-  uint8_t* data = i2cRead(adxlAddress, 0x36,2);     
+  uint8_t* data = i2cRead(adxlAddress, 0x36, 2);
   return (data[0] | (data[1] << 8));
 }
--- IMU6DOF/LPR530AL_LY530ALH_ADXL335/LPR530AL_LY530ALH_ADXL335.ino
+++ IMU6DOF/LPR530AL_LY530ALH_ADXL335/LPR530AL_LY530ALH_ADXL335.ino
@@ -1,15 +1,15 @@
 /* Copyright (C) 2012 Kristian Lauszus, TKJ Electronics. All rights reserved.
- 
+
  This software may be distributed and modified under the terms of the GNU
  General Public License version 2 (GPL2) as published by the Free Software
  Foundation and appearing in the file GPL2.TXT included in the packaging of
  this file. Please note that GPL2 Section 2[b] requires that all works based
  on this software must also be made publicly available under the terms of
  the GPL2 ("Copyleft").
- 
+
  Contact information
  -------------------
- 
+
  Kristian Lauszus, TKJ Electronics
  Web      :  http://www.tkjelectronics.com
  e-mail   :  kristianl@tkjelectronics.com
@@ -46,7 +46,7 @@ void setup() {
   analogReference(EXTERNAL); // 3.3V
   Serial.begin(115200);
   delay(100);//wait for the sensor to get ready
-  
+
   // Calibrate all sensors in horizontal position
   for (uint8_t i = 0; i < 100; i++) { // Take the average of 100 readings
     zeroValue[0] += analogRead(gX);
@@ -56,65 +56,65 @@ void setup() {
     zeroValue[4] += analogRead(aY);
     zeroValue[5] += analogRead(aZ);
     delay(10);
-  }  
+  }
   zeroValue[0] /= 100;
   zeroValue[1] /= 100;
   zeroValue[2] /= 100;
   zeroValue[3] /= 100;
-  zeroValue[4] /= 100;  
+  zeroValue[4] /= 100;
   zeroValue[5] /= 100;
-  zeroValue[5] -= 102.3; // Z value is -1g when facing upwards - Sensitivity = 0.33/3.3*1023=102.3  
-  
+  zeroValue[5] -= 102.3; // Z value is -1g when facing upwards - Sensitivity = 0.33/3.3*1023=102.3
+
   kalmanX.setAngle(180); // Set starting angle
   kalmanY.setAngle(180);
   timer = micros(); // start timing
 }
 
 void loop() {
-  double gyroXrate = -((analogRead(gX)-zeroValue[0])/1.0323); // (gyroXadc-gryoZeroX)/Sensitivity - in quids - Sensitivity = 0.00333/3.3*1023=1.0323
-  gyroXangle += gyroXrate*((double)(micros()-timer)/1000000); // Without any filter
-  
-  double gyroYrate = -((analogRead(gY)-zeroValue[1])/1.0323);
-  gyroYangle += gyroYrate*((double)(micros()-timer)/1000000);
+  double gyroXrate = -((analogRead(gX) - zeroValue[0]) / 1.0323); // (gyroXadc-gryoZeroX)/Sensitivity - in quids - Sensitivity = 0.00333/3.3*1023=1.0323
+  gyroXangle += gyroXrate * ((double)(micros() - timer) / 1000000); // Without any filter
+
+  double gyroYrate = -((analogRead(gY) - zeroValue[1]) / 1.0323);
+  gyroYangle += gyroYrate * ((double)(micros() - timer) / 1000000);
 
   /*
   double gyroZrate = -((analogRead(gZ)-zeroValue[2])/1.0323);
   gyroZangle += gyroZrate*((double)(micros()-timer)/1000000);
   Serial.println(gyroZangle); // This is the yaw
   */
-    
-  double accXval = (double)analogRead(aX)-zeroValue[3];
-  double accYval = (double)analogRead(aY)-zeroValue[4];
-  double accZval = (double)analogRead(aZ)-zeroValue[5]; 
-  
+
+  double accXval = (double)analogRead(aX) - zeroValue[3];
+  double accYval = (double)analogRead(aY) - zeroValue[4];
+  double accZval = (double)analogRead(aZ) - zeroValue[5];
+
   // Convert to 360 degrees resolution
   // atan2 outputs the value of -π to π (radians) - see http://en.wikipedia.org/wiki/Atan2
-  // We are then convert it to 0 to 2π and then from radians to degrees 
-  double accXangle = (atan2(accXval, accZval)+PI)*RAD_TO_DEG;
-  double accYangle = (atan2(accYval, accZval)+PI)*RAD_TO_DEG;
-  
+  // We are then convert it to 0 to 2π and then from radians to degrees
+  double accXangle = (atan2(accXval, accZval) + PI) * RAD_TO_DEG;
+  double accYangle = (atan2(accYval, accZval) + PI) * RAD_TO_DEG;
+
   /* You might have to tune the filters to get the best values */
-  compAngleX = (0.98*(compAngleX+(gyroXrate*(double)(micros()-timer)/1000000)))+(0.02*(accXangle)); 
-  compAngleY = (0.98*(compAngleY+(gyroYrate*(double)(micros()-timer)/1000000)))+(0.02*(accYangle));
-  double xAngle = kalmanX.getAngle(accXangle, gyroXrate, (double)(micros()-timer));
-  double yAngle = kalmanY.getAngle(accYangle, gyroYrate, (double)(micros()-timer));
-  
+  compAngleX = (0.98 * (compAngleX + (gyroXrate * (double)(micros() - timer) / 1000000))) + (0.02 * (accXangle));
+  compAngleY = (0.98 * (compAngleY + (gyroYrate * (double)(micros() - timer) / 1000000))) + (0.02 * (accYangle));
+  double xAngle = kalmanX.getAngle(accXangle, gyroXrate, (double)(micros() - timer));
+  double yAngle = kalmanY.getAngle(accYangle, gyroYrate, (double)(micros() - timer));
+
   timer = micros(); // reset timing
-  
+
   /* print data to processing */
-  Serial.print(gyroXangle);Serial.print("\t");
-  Serial.print(gyroYangle);Serial.print("\t");
-  
-  Serial.print(accXangle);Serial.print("\t");
-  Serial.print(accYangle);Serial.print("\t");
-  
-  Serial.print(compAngleX);Serial.print("\t");  
-  Serial.print(compAngleY); Serial.print("\t"); 
-  
-  Serial.print(xAngle);Serial.print("\t");
-  Serial.print(yAngle);Serial.print("\t");
-   
+  Serial.print(gyroXangle); Serial.print("\t");
+  Serial.print(gyroYangle); Serial.print("\t");
+
+  Serial.print(accXangle); Serial.print("\t");
+  Serial.print(accYangle); Serial.print("\t");
+
+  Serial.print(compAngleX); Serial.print("\t");
+  Serial.print(compAngleY); Serial.print("\t");
+
+  Serial.print(xAngle); Serial.print("\t");
+  Serial.print(yAngle); Serial.print("\t");
+
   Serial.print("\n");
-  
+
   delay(10);
 }
